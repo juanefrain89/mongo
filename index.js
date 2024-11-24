42fa8168d74b6751d5a7257473a1d074a4c0eb2e
juanefrain89 <302420141@ute.edu.mx>
2024-11-24 17:52:10 -0600

    isi2

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ let db; // Variable para almacenar la conexión a la base de datos
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(
+  cors({
+    origin: "*", 
+  })
+);
 
 console.log("Servidor iniciado.");
 
